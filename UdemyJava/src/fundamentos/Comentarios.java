76415b7ac33932accf9e55b5d3722cb9bc7b1b59
hebertsouzaa <hebert.viniciuss@outlook.com.br>
2025-07-13 21:13:33 -0300

    exercicios organização projeto e primeiro programa.

---
+++ UdemyJava/src/fundamentos/Comentarios.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class Comentarios {
+    public static void main(String[] args) {
+        System.out.println("Comentarios no Java são importantes de diversas formas");
+        /**
+         * @author:Hebert Souza
+         * @version:0.1
+         * @since:13/07/2025
+         * Comentario de varias linhas
+         */
+        // Comentario de uma linha
+    }
+}
