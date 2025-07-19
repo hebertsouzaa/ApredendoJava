c632ae2d21f914a4b056ddff79648786c7e39a1b
hebertsouzaa <hebert.viniciuss@outlook.com.br>
2025-07-19 14:16:28 -0300

    Criação do Exercico Area da Circufência.

---
+++ UdemyJava/src/exercicios/AreaCircureferencia.java
@@ -0,0 +1,20 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class AreaCircureferencia {
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Qual é a Área da Circunferência? Informe o raio: "); 
+        double raio = teclado.nextDouble();
+        final double PI = 3.14159;
+
+        double area = PI * raio * raio;
+
+        // Forma correta de exibir a área com unidade
+        System.out.printf("A área é: %.2f Metros%n", area);
+
+        teclado.close();
+    }
+}
